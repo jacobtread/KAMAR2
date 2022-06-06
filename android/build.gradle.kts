b3b83fce18fb809697d4e8a9cef72f62b7c8f7c8
Jacobtread <jacobtread@gmail.com>
2022-06-06 12:16:08 +1200

    Removed unused dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,8 +18,6 @@ dependencies {
 
     implementation("io.ktor:ktor-client-android:$ktorVersion")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-xml:$ktorVersion")
 
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.4.2")
